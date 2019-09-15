7c0b15b3a7d4c6b896e1e5b2512ebe9f4f50f141
Tom Sun <sun_tommy@hotmail.com>
2019-09-14 21:16:21 -0500

    Set up basic Socket.io framework for server-client communication

--- experiments/SocketioExperiment/chat-example/index.js
+++ experiments/SocketioExperiment/chat-example/index.js
@@ -7,10 +7,12 @@ app.get('/', function(req,res){
 });
 
 io.on('connection', function(socket){
-	console.log('a user connected');
+  socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
-
