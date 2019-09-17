0564bd94bc041269815d1c98cf1f84ffe59b4c1c
Matt Dwyer <dwyer@iastate.edu>
2019-09-17 00:38:43 -0500

    messing with relitive paths for testing and eventual deployment to server

--- frontend/res/js/test-app.js
+++ frontend/res/js/test-app.js
@@ -13,7 +13,7 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS('/player-websocket');
+    var socket = new SockJS('http://localhost:8080/player-websocket');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
