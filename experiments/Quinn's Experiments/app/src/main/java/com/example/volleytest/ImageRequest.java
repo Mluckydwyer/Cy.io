f7f06f2dfbc30d8c46ec054ba2caf8397de8fb37
quinn <qdsturm@iastate.edu>
2019-09-27 13:16:01 -0500

    Learning Volley and practicing merge requests. This is the example used in the pdf

---
+++ experiments/Quinn's Experiments/app/src/main/java/com/example/volleytest/ImageRequest.java
@@ -0,0 +1,14 @@
+package com.example.volleytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_request);
+    }
+}
--- experiments/Quinn's Experiments/app/src/main/java/com/example/volleytest/StringRequest.java
+++ experiments/Quinn's Experiments/app/src/main/java/com/example/volleytest/StringRequest.java
@@ -1,14 +1,15 @@
-package com.example.androidtest1;
+package com.example.volleytest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class StringRequest extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_string_request);
     }
+
 }
