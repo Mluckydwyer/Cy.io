8353f288e55b5ad77be6fa3cb6bdbf0bfa211388
jidongs <jidongs@iastate.edu>
2019-09-29 21:41:11 -0500

    Database Connection Established Issue: Can't display query results

--- backend/src/main/java/com/cyio/backend/GameController.java
+++ backend/src/main/java/com/cyio/backend/GameController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GameController {
+
+    @Autowired
+    GameRepository gameRepository; //using autowire to create an instance of blog Repository
 	
 	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping("/restexample")
@@ -21,14 +26,14 @@ public class GameController {
     @CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping("/gamelist")
 	public List<Game> gameList(@RequestParam(value="limit", defaultValue = "10") int limit, @RequestParam(value="title", defaultValue = "Cy.io Game") String title){
-		List<Game> ret = new ArrayList<Game>();
-
-		for (int i = 0; i < limit; i ++) {
-			Game g = new Game(title, UUID.randomUUID(), UUID.randomUUID());
-			ret.add(g);
-		}
-
-		return ret;
+//		List<Game> ret = new ArrayList<Game>();
+//
+//		for (int i = 0; i < limit; i ++) {
+//			Game g = new Game(title, UUID.randomUUID(), UUID.randomUUID());
+//			ret.add(g);
+//		}
+		return gameRepository.findAll();
+		//return ret;
 	}
 
 
