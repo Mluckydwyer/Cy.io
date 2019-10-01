06764077546da4cfac6f31e4de545be067c5a4b2
Matt Dwyer <dwyer@iastate.edu>
2019-10-01 10:46:14 -0500

    temp including build dir to push to server

---
+++ frontend/build/res/js/home.js
@@ -0,0 +1,9 @@
+
+var loginBtn = document.getElementById('login-btn');
+loginBtn.addEventListener('click', function (ev) {
+    addGameToList('Hey, You Added Me!');
+});
+
+function addGameToList(name) {
+
+}
