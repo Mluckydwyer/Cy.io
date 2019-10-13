78037e241c76dd461065af6bc9e73fe2f39c81d7
quinn <qdsturm@iastate.edu>
2019-10-13 14:11:42 -0500

    Added a login page. Need to still have it communicate with the server v2

--- Android/PlaceHolder/app/src/androidTest/java/com/example/placeholder/ExampleInstrumentedTest.java
+++ Android/PlaceHolder/app/src/androidTest/java/com/example/placeholder/ExampleInstrumentedTest.java
@@ -23,6 +23,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.CyIO", appContext.getPackageName());
+        assertEquals("com.example.placeholder", appContext.getPackageName());
     }
 }
