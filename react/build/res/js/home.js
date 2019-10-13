bfe2745dbf26e452199ca107705938e902bdae7b
Matt Dwyer <dwyer@iastate.edu>
2019-10-13 00:50:21 -0500

    Removing uneeded files

--- react/build/res/js/home.js
+++
@@ -1,9 +0,0 @@
-
-var loginBtn = document.getElementById('login-btn');
-loginBtn.addEventListener('click', function (ev) {
-    addGameToList('Hey, You Added Me!');
-});
-
-function addGameToList(name) {
-
-}
