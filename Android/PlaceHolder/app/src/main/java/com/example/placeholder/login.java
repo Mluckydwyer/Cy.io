c9f3c4184c43dd2a6401b4b1c3136cb65227a086
quinn <qdsturm@iastate.edu>
2019-10-20 19:01:52 -0500

    working login

--- Android/PlaceHolder/app/src/main/java/com/example/placeholder/login.java
+++ Android/PlaceHolder/app/src/main/java/com/example/placeholder/login.java
@@ -8,12 +8,25 @@ import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.placeholder.app.AppController;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class login extends AppCompatActivity
 {
     Button login;
     EditText username, password;
+    TextView vt;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -22,6 +35,7 @@ public class login extends AppCompatActivity
         login = (Button)findViewById(R.id.login);
         username = (EditText)findViewById(R.id.usernameEntry);
         password = (EditText)findViewById(R.id.passwordEntry);
+        vt = (TextView) findViewById(R.id.tv1);
         //GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
 
         login.setOnClickListener(new View.OnClickListener()
@@ -29,10 +43,47 @@ public class login extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                if(username.getText().toString().equals("a username object from online") && password.getText().toString().equals("password object"))
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String tag_json_obj ="json_obj_req";
+
+                JSONObject obj = new JSONObject();
+                try
                 {
-                    openHomePage();
+                    obj.put("userName", user);
+                    obj.put("password", pass);
                 }
+                catch (JSONException e)
+                {
+                    System.out.println(e.getMessage());
+                }
+                String URL = "http://coms-309-nv-4.misc.iastate.edu:8081/auth/login";
+                JsonObjectRequest jobj = new JsonObjectRequest(Request.Method.POST, URL, obj, new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response)
+                    {
+                        JSONArray tokens = response.names();
+                        vt.setTextSize(15);
+                        try
+                        {
+                            vt.setText("work please");
+                            vt.setText(response.getString(tokens.get(0).toString()) + response.getString(tokens.get(1).toString()));
+                        }
+                        catch (JSONException e)
+                        {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                }, new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        VolleyLog.d("Login","Error: "+ error.getMessage());
+                    }
+                });
+                AppController.getInstance().addToRequestQueue(jobj, tag_json_obj);
             }
         });
 
